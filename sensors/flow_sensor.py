7e4e999cb8f81bf86c9d401b93c47f4e4770cad5
lesefe <sefe.leandro@gmail.com>
2018-04-17 15:26:39 +0200

    pujada main

--- sensors/flow_sensor.py
+++ sensors/flow_sensor.py
@@ -4,32 +4,35 @@ import RPi.GPIO as GPIO
 #Importem llibreries necesaries
 import time, sys
 
-#Definim pin BCM 17 per connectarhi el sensor de flux
-FLOW_SENSOR = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+from .main import Sensor
 
-#Variable global per contar voltes del sensor de flux
-global count
-count = 0
+class flowsensor(object):
+    def __init__(self,name):
+        super(flowsensor, self).__init__(name)
+#Defenim pin BCM17 per conectar el sensor de flux
+    FLOW_SENSOR = 17
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#Varialble global per contar voltes del sensor de flux
+    global count
+    count = 0
+#Funcio que llegeig les voltes del pin 17 i les transforma a litres
+    def countPulse(channel):
+       global count
+       count = count+1
+       actual_liters = count/float(400)
+       #print count
+       print actual_liters
+       return actual_liters
 
-#Funció que llegeig les voltes del pin 17 i les transfotma a litres
-def countPulse(channel):
-   global count
-   count = count+1
-   actual_liters = count/float(400)
-   #print count
-   print actual_liters
-   return actual_liters
+    GPIO.add_event_detect(FLOW_SENSOR, GPIO.RISING, callback=countPulse)
+    actual_liters = actual_liters
 
-GPIO.add_event_detect(FLOW_SENSOR, GPIO.RISING, callback=countPulse)
-actual_liters = actual_liters
-
-#bucle que mante en espera perque conti fins que ho interrumpim amb CTRL+C
-while True:
-    try:
-        time.sleep(1)
-    except KeyboardInterrupt:
-        print '\ncaught keyboard interrupt!, bye'
-        GPIO.cleanup()
-        sys.exit()
+    #bucle que mante en espera perque conti fins que ho interrumpim amb CTRL+C
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print '\ncaught keyboard interrupt!, bye'
+            GPIO.cleanup()
+            sys.exit()
