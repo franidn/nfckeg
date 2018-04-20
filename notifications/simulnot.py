3c3712503afac915b2565063ba579cf69ed9a3b5
lesefe <sefe.leandro@gmail.com>
2018-04-20 16:02:09 +0200

    pujada flow sensor bo

--- notifications/simulnot.py
+++ notifications/simulnot.py
@@ -2,6 +2,17 @@ from main import Notification
 
 class MockNotification(Notification):
 
+        def __init__(self):
+            super(MockNotification, self).__init__()
+
         def notify(self, string):
-            #print("*****"+string+"*****")
-            print("hola")
+            print("*****"+string+"*****")
+
+if __name__ == "__main__":
+    notify.message = self.message
+    while True:
+        try:
+            self.mesage = "hola"
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print '\ncaught keyboard interrupt!, bye'
