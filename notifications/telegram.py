aa16f38aa86d096123ef516ad6cfc1f793c7c43c
lesefe <sefe.leandro@gmail.com>
2018-04-21 15:42:13 +0200

    com la lieu

--- notifications/telegram.py
+++ notifications/telegram.py
@@ -6,11 +6,11 @@ class TelegramNotification(Notification):
     def __init__(self):
         super(TelegramNotification, self).__init__()
         self.bot = telepot.Bot('536095288:AAFc4ojD_f4clr6tHXroO33E6qzB3FYM8AM')
-        self.chat_id = "-217126603"
+        self.chat_id = "451372203"
 
     def notify(self, string):
         print("*****"+string+"*****")
-        bot.sendMessage(self.chat_id, "NOT: "+string)
+        self.bot.sendMessage(self.chat_id, "NOT: "+string)
 
 if __name__ == "__main__":
     message = TelegramNotification()
